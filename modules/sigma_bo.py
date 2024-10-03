bf600eaa7e8008379b7337f0977019b2ba324648
idirdehmous@gmail.com <idirdehmous@gmail.com>
2024-10-03 10:27:20 +0000

    Add a class for writing sigma ratios by date and time

--- modules/sigma_bo.py
+++ modules/sigma_bo.py
@@ -8,7 +8,7 @@ from  .setting_env import TuneEnv
 
 # BASED ON THE PROGRAM ratio.F90  IN THE ORIGINAL VERSION  B. Strajnar 2010/10/26
 
-class Ratios(object):
+class AverageRatios(object):
       def __init__(self,paths  =None ,
                       Nobs   =None ,
                       rednmc =None ,
@@ -153,10 +153,15 @@ class Predef:
           # INIT LISTS 
           self.psigma=[]     ;
           self.dates =dates  ;
-          self.rabso =9.9E-310  ; # MISSING DATA 
-          
+          self.rabso =9.9E-310  ; # MISSING DATA           
           return None
 
+     
+      def NotNone (self,item):
+          if item != None:
+             return True
+          else:
+             return False
 
       def Write2File (self , varname , pname , value ):
           # OUT PATH ( BASEDIR/out)
@@ -186,33 +191,31 @@ class Predef:
                print("UNKOWN PARAMATER -->",param , " POSSIBLE PARAM SHORT NAME : t , bt , q and ke" )
                sys.exit ()
           for  dt in self.dates:
-       
-               self.psigma=self.psigma + Odb.ReadMandalay (self.basedir, dt, "predef")[idx]
-               # GET MEAN FOR EACH DATE/TIME 
-               if len( self.psigma) != 0:
-                  if   param == "t": 
-                       so_pred_dict[dt]  = self.psigma
-                  elif param == "bt": 
-                       so_pred_dict[dt]  = self.psigma
-                  elif param == "q" :
-                       so_pred_dict[dt]  = self.psigma
-                  elif param == "ke":
-                       so_pred_dict[dt]  = self.psigma
+              if  self.NotNone(Odb.ReadMandalay (self.basedir, dt, "predef")[idx] ):
+                  self.psigma=self.psigma + Odb.ReadMandalay (self.basedir, dt, "predef")[idx]
+                  # GET MEAN FOR EACH DATE/TIME 
+                  if len( self.psigma) != 0:
+                     if   param == "t": 
+                         so_pred_dict[dt]  = mean(self.psigma)
+                     elif param == "bt": 
+                         so_pred_dict[dt]  = mean(self.psigma)
+                     elif param == "q" :
+                         so_pred_dict[dt]  = mean(self.psigma)
+                     elif param == "ke":
+                         so_pred_dict[dt]  = mean(self.psigma)
 
+                     means.append( dt+"    "+str( mean(self.psigma)) )
+                  else:
+                     if   param == "t":
+                         so_pred_dict[dt]  = self.rabso
+                     elif param == "bt":
+                         so_pred_dict[dt]  = self.rabso
+                     elif param == "q" :
+                         so_pred_dict[dt]  = self.rabso
+                     elif param == "ke":
+                         so_pred_dict[dt]  = self.rabso
 
-                  #dt ,  mean(self.psigma) , param )
-                  means.append( dt+"    "+str( mean(self.psigma)) )
-               else:
-                  if   param == "t":
-                       so_pred_dict[dt]  = self.rabso
-                  elif param == "bt":
-                       so_pred_dict[dt]  = self.rabso
-                  elif param == "q" :
-                       so_pred_dict[dt]  = self.rabso
-                  elif param == "ke":
-                       so_pred_dict[dt]  = self.rabso
-
-                  means.append( dt+"    "+"None")
+                     means.append( dt+"    "+"None")
 
           if self.lwrite == True:
              self.Write2File ("so_pred_means_vs_date" ,param , means  )
@@ -237,17 +240,20 @@ class Diag:
             #    R     = d(o,a) * d(b,o)^T
 
       """
-      def __init__(self ,paths , dates, lverb , lwrite):
-          self.basedir=paths["BASEDIR"]
-          self.lverb   =lverb 
-          self.lwrite  =lwrite 
+      def __init__(self ,paths =None , 
+                         dates =None , 
+                         lverb =None , 
+                         lwrite=None):
+          if paths != None:  self.basedir=paths["BASEDIR"]
+          if dates != None:  self.dates  =dates
+          if lverb != None:  self.lverb  =lverb 
+          if lwrite!= None:  self.lwrite =lwrite 
 
           # INIT LISTS 
           self.sigb  =[]    ;
           self.sigo  =[]    ;
           self.sigb_out=[]  ;
           self.sigo_out=[]  ;
-          self.dates =dates ; 
           self.rabso =9.9E-310 ;  # MISSING DATA 
           return None  
 
@@ -266,9 +272,16 @@ class Diag:
              outfile.write(str(value) )
 
 
+      def NotNone (self,item):
+          if item != None:
+             return True
+          else:
+             return False
+
+
       def ComputeSigmab(self,fg_dep , an_dep):
           s=0
-          if len(fg_dep )!=0 and len(an_dep) !=0:
+          if self.NotNone(fg_dep) and len(fg_dep )!=0 and self.NotNone(an_dep) and len(an_dep) !=0:
              for i in range(len(fg_dep )): 
                   diff=fg_dep[i]-an_dep[i] 
                   s   =s+ diff  *fg_dep[i]
@@ -280,7 +293,7 @@ class Diag:
 
       def ComputeSigmao(self,fg_dep , an_dep):
           s=0
-          if len(fg_dep) != 0:
+          if self.NotNone(fg_dep) and len(fg_dep )!=0 and self.NotNone(an_dep) and len(an_dep) !=0:
              for i in range(len(fg_dep)): 
                  s=s + (fg_dep[i]*an_dep[i] )
              sigmao = sqrt( s/len(fg_dep))
@@ -308,25 +321,22 @@ class Diag:
                   self.sigo.append(self.ComputeSigmao( fg, an ))   # OBSERVATIONS  
                   self.sigb.append(self.ComputeSigmab( fg, an ))   # FIRST GUESS 
                   if  param  =="t" : 
-                     diag_so_dict[dt] = self.ComputeSigmao( fg, an )
-                     diag_sb_dict[dt] = self.ComputeSigmab( fg, an )
+                     diag_so_dict[dt] = [fg,an]
+                     diag_sb_dict[dt] = [fg,an]
                   elif param =="bt":
-                     diag_so_dict[dt] = self.ComputeSigmao( fg, an )
-                     diag_sb_dict[dt] = self.ComputeSigmab( fg, an )
-
+                     diag_so_dict[dt] = [fg,an]
+                     diag_sb_dict[dt] = [fg,an]
                   elif param =="q" :
-                     diag_so_dict[dt] = self.ComputeSigmao( fg, an )
-                     diag_sb_dict[dt] = self.ComputeSigmab( fg, an )
-
+                     diag_so_dict[dt] = [fg,an]
+                     diag_sb_dict[dt] = [fg,an]
                   elif param == "ke":
-                     diag_so_dict[dt] = self.ComputeSigmao( fg, an )
-                     diag_sb_dict[dt] = self.ComputeSigmab( fg, an )
-
+                     diag_so_dict[dt] = [fg,an]
+                     diag_sb_dict[dt] = [fg,an]
                   else:
                      diag_so_dict[dt] = self.rabso
                      diag_sb_dict[dt] = self.rabso
 
-
+#
                   if self.lwrite==True:
                      self.sigo_out.append(dt+"    "+str(self.ComputeSigmao( fg, an )) )
                      self.sigb_out.append(dt+"    "+str(self.ComputeSigmab( fg, an )) )
@@ -339,7 +349,6 @@ class Diag:
                   if self.lwrite ==True:
                      self.sigo_out.append(dt+"    "+"  None" )
                      self.sigb_out.append(dt+"    "+"  None" )
-        
           if self.lwrite==True:
              self.Write2File("so_diag_vs_date", param , self.sigo_out)
              self.Write2File("sb_diag_vs_date", param , self.sigb_out)
@@ -357,41 +366,81 @@ class Diag:
 
 
 
+dict_rso={}
+dict_rsb={}
+                                
+class RatiosByCycle:
+
+    d=Diag(None,None ,None ,None)
+    def __init__(self, PathDict,  cdtg, 
+                       rednmc  ,  so_pred_d ,
+                       so_diag_d, sb_pred_d , 
+                       sb_diag_d  , Nobs):
 
 
-class RatiosByDate:
-    def __init__(self, paths, dates, so_pred):
 #        Ratios.__init__(self )
-        self.so_pred = so_pred 
-        print( self.so_pred)  
-        return None 
+        self.cdtg    = cdtg
+        self.ptot    =  sum(Nobs)
+        self.pt      =  Nobs[0]
+        self.pbt     =  Nobs[1]
+        self.pq      =  Nobs[2]
+        self.pke     =  Nobs[3]
 
-#    def GlobalRatio (self ):
-#        print( r.rednmc  )
+        # SIGMA O/B PREDEF 
+        self.so_tp   = so_pred_d[0]   # p=predefined 
+        self.so_btp  = so_pred_d[1]
+        self.so_qp   = so_pred_d[2]
+        self.so_kep  = so_pred_d[3]
+    
+        self.sb_tp   = sb_pred_d[0]
+        self.sb_qp   = sb_pred_d[1]
+        self.sb_kep  = sb_pred_d[2]
+
+
+        # SIGMA O/B DIAG
+        self.so_td   = so_diag_d[0]   # d=diagnosed 
+        self.so_btd  = so_diag_d[1]
+        self.so_qd   = so_diag_d[2]
+        self.so_ked  = so_diag_d[3]
+
+        self.sb_td   = sb_diag_d[0]
+        self.sb_btd  = sb_diag_d[1]
+        self.sb_qd   = sb_diag_d[2]
+        self.sb_ked  = sb_diag_d[3]
+
         
+        self.rednmc  = rednmc     
 
-#class RatiosByDate(Ratios):
-#    r=Ratios (paths  = {"BASEDIR":None} ,
-#                      Nobs   =None ,
-#                      rednmc =None ,
-#                      so_pred=None ,
-#                      so_diag=None ,
-#                      sb_pred=None ,
-#                      sb_diag=None ,
-#                      lwrite =None )
+        self.rabso =9.9E-310  ; # MISSING DATA
 
-#    def __init___(self ):
-        #super().__init__(rednmc)
-        #self.rednmc   =rednmc 
-        #print("-----", self.rednmc )
-#        self.so_pred  =so_pred
-#        self.so_diag  =so_diag 
+        self.rso=None
+        self.rsb=None
+        #self.rso_dicts=[]
+        #self.rsb_dicts=[]
+        return None
 
-#        self.sb_pred  =sb_pred
-#        self.sb_diag  =sb_diag
-#        return None 
 
-"""    def ComputeRatios(self, pred , diag , rednmc,  target  ):
+    def NotNone (self, item):
+        if item != None:
+           return True
+        else:
+           return False 
+
+    def CheckInstance(self, var):
+        if isinstance( var , list ):
+           return True
+        else:
+           return False 
+    def CatchExcept(self, _dict, _key):
+        lst=[]
+        try:
+           lst= _dict[_key]
+           return lst
+        except:
+           KeyError
+           return None 
+      
+    def ComputeRatios(self, pred , diag , rednmc,  target  ):
         if pred != self.rabso  and diag != self.rabso :
            if target   == "sigmao":
               ratio =diag/pred
@@ -399,7 +448,109 @@ class RatiosByDate:
            elif target == "sigmab":
               ratio =diag/(pred * float(rednmc))
               return ratio
-        else:
-           ratio =None
-           return ratio"""
- 
+           else:
+              ratio =None
+              return ratio
+
+    def GetByDate(self):
+        """
+        Rso = sqrt( (roq**2 * pq /nobs  +rot**2*pt/nobs + robt**2*pbt/nobs + roke**2*pke/nobs  ) )
+        """
+        dict_rso={}
+        dict_rsb={}
+
+        for dt in self.cdtg:
+            # NOBS FOR EACH OBS SUBSET 
+            
+            lst_tp   = self.CatchExcept(self.so_tp ,dt ) 
+            lst_btp  = self.CatchExcept(self.so_btp,dt )  
+            lst_qp   = self.CatchExcept(self.so_qp ,dt )  
+            lst_kep  = self.CatchExcept(self.so_kep,dt )  
+
+            if self.CheckInstance(lst_tp  ): self.pt =len(lst_tp  )
+            if self.CheckInstance(lst_btp ): self.pbt=len(lst_btp )
+            if self.CheckInstance(lst_qp  ): self.pq =len(lst_qp  )
+            if self.CheckInstance(lst_kep ): self.pke=len(lst_kep )
+  
+            lst_td   = self.CatchExcept(self.so_td , dt  )
+            lst_btd  = self.CatchExcept(self.so_btd, dt  )
+            lst_qd   = self.CatchExcept(self.so_qd , dt  )
+            lst_ked  = self.CatchExcept(self.so_ked, dt  )
+
+                   
+            #  SIGMA DIAG  BY DATE/CYCLE                             FG     , # AN 
+            if self.NotNone(lst_td)  : 
+               sigo_td = self.d.ComputeSigmao( lst_td [0] , lst_td [1] )
+            else:
+               sigo_td=self.rabso
+            if self.NotNone(lst_btd) : 
+               sigo_btd= self.d.ComputeSigmao( lst_btd[0] , lst_btd[1] )
+            else:
+               sigo_btd=self.rabso
+            if self.NotNone(lst_qd)  : 
+               sigo_qd = self.d.ComputeSigmao( lst_qd [0] , lst_qd [1] )
+            else:
+               sigo_qd = self.rabso
+    
+            if self.NotNone(lst_ked) : 
+               sigo_ked= self.d.ComputeSigmao( lst_ked[0] , lst_ked[1] )
+            else:
+               sigo_ked= self.rabso 
+
+
+
+
+            # SIGMA B DIAG BY DATE/CYCLE  
+            if self.NotNone(lst_td)  :
+               sigb_td = self.d.ComputeSigmab( lst_td [0] , lst_td [1] )
+            else:
+               sigb_td=self.rabso
+            if self.NotNone(lst_btd) :
+               sigb_btd= self.d.ComputeSigmab( lst_btd[0] , lst_btd[1] )
+            else:
+               sigb_btd=self.rabso
+            if self.NotNone(lst_qd)  :
+               sigb_qd = self.d.ComputeSigmab( lst_qd [0] , lst_qd [1] )
+            else:
+               sigb_qd = self.rabso
+
+            if self.NotNone(lst_ked) :
+               sigb_ked= self.d.ComputeSigmab( lst_ked[0] , lst_ked[1] )
+            else:
+               sigb_ked= self.rabso
+
+
+
+            # OBS RATIOS FOR EACH DATA/CYCLE             
+            target = "sigmao"
+            rot = self.ComputeRatios( self.so_tp [dt]  , sigo_td   ,self.rednmc , target )
+            robt= self.ComputeRatios( self.so_btp[dt]  , sigo_btd  ,self.rednmc , target )
+            roq = self.ComputeRatios( self.so_qp [dt]  , sigo_qd   ,self.rednmc , target )
+            roke= self.ComputeRatios( self.so_kep[dt]  , sigo_ked  ,self.rednmc , target )
+            # THE OBS WEIGHT IS ZERO IF NO OBS (CAN SET IT TO rabso , BUT WILL BE EVEN MULTIPLIED BY pt=0)
+            if not self.NotNone(rot ): rot =0 ; pt =0
+            if not self.NotNone(robt): robt=0 ; pbt=0
+            if not self.NotNone(roq ): roq =0 ; qt =0
+            if not self.NotNone(roke): roke=0 ; ket=0
+
+            # Rso AVG 
+            self.rso = sqrt( (rot**2 * self.pt /self.ptot  +robt**2* self.pbt/self.ptot + roq**2*self.pq/self.ptot + roke**2* self.pke/self.ptot  ) )
+
+            # B RATIOS FOR EACH DATA/CYCLE
+            target="sigmab"
+
+            rbt  = self.ComputeRatios( self.sb_tp  , sigb_td    ,self.rednmc , target )
+            rbq  = self.ComputeRatios( self.sb_qp  , sigb_qd    ,self.rednmc , target )
+            rbke = self.ComputeRatios( self.sb_kep , sigb_ked   ,self.rednmc , target )
+
+      
+            # Rrb AVG 
+            if not self.NotNone(rbt ): rbt =0 ; pt =0
+            if not self.NotNone(rbq ): rbq =0 ; qt =0
+            if not self.NotNone(rbke): rbke=0 ; ket=0
+
+            self.rsb =sqrt (rbt**2 *(self.pt /self.ptot ) + rbq**2 *(self.pq /self.ptot ) + rbke**2*(self.pke/self.ptot ) )
+
+            dict_rso[dt]= self.rso 
+            dict_rsb[dt]= self.rsb
+        return  dict_rso , dict_rsb 
